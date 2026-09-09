a596b34e28cca0ba5cd1108a8e0df3395b5436e1
xmas9441-dev <xmas9441@gmail.com>
2026-09-09 14:45:00 +0300

    Update app_export.dart

--- lib/core/app_export.dart
+++ lib/core/app_export.dart
@@ -1,65 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:sizer/sizer.dart';
-
-import '../core/app_export.dart';
-import '../widgets/custom_error_widget.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  bool hasShownError = false;
-
-  // 🚨 CRITICAL: Custom error handling - DO NOT REMOVE
-  ErrorWidget.builder = (FlutterErrorDetails details) {
-    if (!hasShownError) {
-      hasShownError = true;
-
-      // Reset flag after 3 seconds to allow error widget on new screens
-      Future.delayed(Duration(seconds: 5), () {
-        hasShownError = false;
-      });
-
-      return CustomErrorWidget(errorDetails: details);
-    }
-    return SizedBox.shrink();
-  };
-
-  // 🚨 CRITICAL: Device orientation lock - DO NOT REMOVE
-  Future.wait([
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]),
-  ]).then((value) {
-    GoRouter.optionURLReflectsImperativeAPIs = true;
-    runApp(MyApp());
-  });
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Sizer(
-      builder: (context, orientation, screenType) {
-        return MaterialApp.router(
-          title: 'balyiq',
-          theme: AppTheme.lightTheme,
-          darkTheme: AppTheme.darkTheme,
-          themeMode: ThemeMode.light,
-          // 🚨 CRITICAL: NEVER REMOVE OR MODIFY
-          builder: (context, child) {
-            return MediaQuery(
-              data: MediaQuery.of(
-                context,
-              ).copyWith(textScaler: TextScaler.linear(1.0)),
-              child: child!,
-            );
-          },
-          // 🚨 END CRITICAL SECTION
-          debugShowCheckedModeBanner: false,
-          routerConfig: appRouter,
-        );
-      },
-    );
-  }
-}
+export 'package:connectivity_plus/connectivity_plus.dart';
+export 'package:google_fonts/google_fonts.dart';
+export 'package:go_router/go_router.dart';
+export '../routes/app_routes.dart';
+export '../widgets/custom_icon_widget.dart';
+export '../widgets/custom_image_widget.dart';
+export '../theme/app_theme.dart';
